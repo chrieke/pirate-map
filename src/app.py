f80a08627efcb071b4616ddb8433b1e9b8755ad8
Christoph Rieke <christoph.k.rieke@up42.com>
2020-12-21 03:10:24 +0100

    default to dict,

--- src/app.py
+++ src/app.py
@@ -2,8 +2,20 @@ from pathlib import Path
 import streamlit as st
 
 from main import render
+# value, min, max,
+DEFAULT_CONFIG = {
+    "water": {"status": True, "area": [-4, -50, 5, 1], "color": "#2185C5"},
+    "sand": {"status": True, "area": [-4, -40, 5, 1], "color": "#FFFFA6"},
+    "grass": {"status": True, "area": [-8, -40, 5, 1], "color": "#BDF271"},
+    "gravel": {"status": True, "area": [-10, -20, 5, 1], "color": "#CFC291"},
+}
 
+# st.set_page_config(layout="wide")
 st.title("Pirate Map")
+sec1, sec2 = st.beta_columns([2, 1])
+session = SessionState.get(
+    run_id=0
+)  # see https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
 
 display_which = st.radio(
     "Display", options=["Sand", "Grass", "Gravel"], key="display_which"
